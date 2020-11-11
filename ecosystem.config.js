507b2a071639a3610f2244ca27b1ab2165d3c4e7
Connor Tupper <connor@connorontheweb.com>
2020-11-11 14:29:01 -0800

    changed server stuff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [
       {
         name: 'nuxt-cloudways-test',
-        port: 4000,
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
